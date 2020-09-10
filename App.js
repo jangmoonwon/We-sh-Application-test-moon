26c5efbd5bb5d32e30c25528b61401a4dffd096b
MOON <muneon96@naver.com>
2020-09-10 22:59:18 +0900

    [HomeScreen] differenceData 값 수정, Podfile 생성

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Text, View } from "react-native";
 import styled from "styled-components";
-import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 function HomeScreen() {
   const year = new Date().getFullYear() + "년" + " ";
@@ -16,69 +16,64 @@ function HomeScreen() {
   todayData.setMinutes(0);
   todayData.setSeconds(0, 0);
 
-  const anniversaryData = new Date(2015, 8, 9);
+  const anniversaryData = new Date('2015/8/9');
   const differenceData =
-    (todayData - anniversaryData) / (1000 * 60 * 60 * 24) + 31 + 1;
-
-  return (
+    (todayData - anniversaryData) / (1000 * 60 * 60 * 24) + 1;
+  return(
     <ScreenContainer>
-      <ScreenText>Home Pages</ScreenText>
+      <ScreenText>Home Screen</ScreenText>
       <ScreenText>{today}</ScreenText>
       <ScreenText>D + {differenceData}</ScreenText>
     </ScreenContainer>
   );
 }
 
-
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings</Text>
-    </View>
+function AlbumScreen() {
+  return(
+    <ScreenContainer>
+      <ScreenText>Album Screen</ScreenText>
+    </ScreenContainer>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
+function WeshBottomTab() {
+  return(
+    <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: '#e85433',
+    }}
+    >
+      <Tab.Screen 
+      name="home"
+      component={HomeScreen}
+      options={{
+        tabBarLabel: "Home",
+        tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+      />
+      <Tab.Screen 
+      name="album"
+      component={AlbumScreen}
+      options={{
+        tabBarLabel: "Album",
+        tabBarIcon: ({color, size}) => (
+          <Ionicons name="ios-albums" color={color} size={size}  />
+        ),
+      }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            if (route.name === "Home") {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? "ios-information-circle"
-                      : "ios-information-circle-outline"
-                  }
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === "Settings") {
-              return (
-                <Ionicons
-                  name={focused ? "ios-list-box" : "ios-list"}
-                  size={size}
-                  color={color}
-                />
-              );
-            }
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "blue",
-          inactiveTintColor: "gray",
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+   <NavigationContainer>
+     <WeshBottomTab />
+   </NavigationContainer>
   );
 }
 
@@ -89,7 +84,7 @@ const ScreenContainer = styled.View`
 `;
 
 const ScreenText = styled.Text`
-font-size: 30px;
-font-weight: 500;
-margin: 5px;
+  font-size: 30px;
+  font-weight: 500;
+  margin: 5px;
 `;
