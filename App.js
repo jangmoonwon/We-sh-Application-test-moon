6808faa8704243c621307381a4d9e3cdba54878b
MOON <muneon96@naver.com>
2020-09-11 23:11:51 +0900

    [AlbumScreen] ImagePicker 설치

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 function HomeScreen() {
   const year = new Date().getFullYear() + "년" + " ";
@@ -16,10 +18,10 @@ function HomeScreen() {
   todayData.setMinutes(0);
   todayData.setSeconds(0, 0);
 
-  const anniversaryData = new Date('2015/8/9');
+  const anniversaryData = new Date("2015/8/9");
   const differenceData =
     (todayData - anniversaryData) / (1000 * 60 * 60 * 24) + 1;
-  return(
+  return (
     <ScreenContainer>
       <ScreenText>Home Screen</ScreenText>
       <ScreenText>{today}</ScreenText>
@@ -29,41 +31,94 @@ function HomeScreen() {
 }
 
 function AlbumScreen() {
-  return(
-    <ScreenContainer>
-      <ScreenText>Album Screen</ScreenText>
-    </ScreenContainer>
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  const openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>사진 추가하기</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  thumbnail: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+});
 
 const Tab = createBottomTabNavigator();
 
 function WeshBottomTab() {
-  return(
+  return (
     <Tab.Navigator
-    tabBarOptions={{
-      activeTintColor: '#e85433',
-    }}
+      tabBarOptions={{
+        activeTintColor: "#e85433",
+      }}
     >
-      <Tab.Screen 
-      name="home"
-      component={HomeScreen}
-      options={{
-        tabBarLabel: "Home",
-        tabBarIcon: ({color, size}) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-      }}
+      <Tab.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
       />
-      <Tab.Screen 
-      name="album"
-      component={AlbumScreen}
-      options={{
-        tabBarLabel: "Album",
-        tabBarIcon: ({color, size}) => (
-          <Ionicons name="ios-albums" color={color} size={size}  />
-        ),
-      }}
+      <Tab.Screen
+        name="album"
+        component={AlbumScreen}
+        options={{
+          tabBarLabel: "Album",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-albums" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
@@ -71,9 +126,9 @@ function WeshBottomTab() {
 
 export default function App() {
   return (
-   <NavigationContainer>
-     <WeshBottomTab />
-   </NavigationContainer>
+    <NavigationContainer>
+      <WeshBottomTab />
+    </NavigationContainer>
   );
 }
 
